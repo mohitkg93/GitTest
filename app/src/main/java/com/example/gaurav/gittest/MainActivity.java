844b734e5e914f5fa6fd78711295eefd96845cb9
kumar gaurav <kumar gaurav>
2017-02-18 18:26:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gaurav/gittest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gaurav.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testGit()
+    {
+        int i=1;
+        int ii=i+i;
+    }
+}
